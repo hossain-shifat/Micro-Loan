217fea87c56962e876c8ea3da9b6445ee48f53fa
Hossain-Shifat <161940464+hossain-shifat@users.noreply.github.com>
2025-12-06 11:12:46 +0600

    Add how ii work section

--- src/Pages/Home/HowItWorks/HowItWorks.jsx
+++ src/Pages/Home/HowItWorks/HowItWorks.jsx
@@ -10,8 +10,8 @@ const HowItWorks = () => {
             <div className="space-y-4">
                 {
                     process.map((step, index) => (
-                        <div className={`flex flex-col gap-4 items-center ${index % 2 !== 0 ? "md:flex-row-reverse md:px-10" : "md:flex-row md:px-10"}`}>
-                            <div className="w-full md:w-90">
+                        <div className={`flex flex-col gap-4 md:gap-20 items-center ${index % 2 !== 0 ? "md:flex-row-reverse md:px-10" : "md:flex-row md:px-10"}`}>
+                            <div className="w-full md:w-80">
                                 <img className="w-full h-auto object-cover rounded-xl p-4 md:p-0" src={step.img} alt="" />
                             </div>
                             <div className="space-y-4 p-4 max-w-[600px]">
