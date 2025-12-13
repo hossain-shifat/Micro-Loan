250d0b2b4800e702c423912bef27ab15ece4ace5
Hossain-Shifat <161940464+hossain-shifat@users.noreply.github.com>
2025-12-13 09:36:44 +0600

    Add some fixes in admin stats

--- src/Pages/Dashboard/Dashboard/AdminDashboard/AdminPieChart/AdminPieChart.jsx
+++ src/Pages/Dashboard/Dashboard/AdminDashboard/AdminPieChart/AdminPieChart.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
 import useAxiosSecure from '../../../../../Hooks/Axios/AxiosSecure/useAxiosSecure'
 import { useQuery } from '@tanstack/react-query'
-import { PieChart, Pie, Cell, Tooltip, Legend } from "recharts";
+import { PieChart, Pie, Cell, Tooltip, Legend, ResponsiveContainer } from "recharts";
 import useAuth from '../../../../../Hooks/UseAuth/useAuth';
 import Loading from '../../../../../Components/Loading/Loading';
+import FadeIn from '../../../../../Components/Animations/FadeIn/FadeIn';
 
 const AdminPieChart = () => {
 
@@ -32,49 +33,56 @@ const AdminPieChart = () => {
     return (
         <div className="space-y-5 ">
             <div className="flex flex-col md:flex-row *:p-5 *:md:p-10 *:border *:border-base-100 *:bg-base-300/45 *:rounded-2xl *:shadow-sm *:shadow-base-100 gap-5 *:min-h-[10vh]">
-                <div className="flex-1 grid gap-5 justify-center items-center max-w-dvw w-full mx-auto">
-                    <div>
-                        <h1 className="font-bold text-2xl">User Role Statistics</h1>
+                <FadeIn>
+                    <div className="flex-1 grid gap-5 justify-center items-center max-w-dvw w-full mx-auto">
+                        <div>
+                            <h1 className="font-bold text-2xl">User Role Statistics</h1>
+                        </div>
+                        <div style={{ width: '100%', height: 350 }}>
+                            <ResponsiveContainer>
+                                <PieChart>
+                                    <Pie data={pieData} dataKey="value" nameKey="name" outerRadius='100%' fill="#8884d8" label isAnimationActive={true} >
+                                        {pieData.map((entry, index) => (
+                                            <Cell key={index} fill={COLORS[index % COLORS.length]} />
+                                        ))}
+                                    </Pie>
+                                    <Tooltip />
+                                    <Legend />
+                                </PieChart>
+                            </ResponsiveContainer>
+                        </div>
                     </div>
-                    <PieChart style={{ width: "100%", maxWidth: "900px", height: "550px" }}>
-                        <Pie data={pieData} dataKey="value" nameKey="name" outerRadius='100%' fill="#8884d8" label isAnimationActive={true} >
-                            {pieData.map((entry, index) => (
-                                <Cell key={index} fill={COLORS[index % COLORS.length]} />
-                            ))}
-                        </Pie>
-                        <Tooltip />
-                        <Legend />
-                    </PieChart>
-                </div>
-
-                <div className="flex-1 grid gap-5">
-                    <div>
-                        <h1 className="font-bold text-2xl text-center">Recent Users</h1>
+                </FadeIn>
+                <FadeIn>
+                    <div className="flex-1 grid gap-5">
+                        <div>
+                            <h1 className="font-bold text-2xl text-center">Recent Users</h1>
+                        </div>
+                        <div className="overflow-x-auto no-scrollbar">
+                            <table className="table">
+                                {/* head */}
+                                <thead>
+                                    <tr>
+                                        <th>Name</th>
+                                        <th>Email</th>
+                                        <th>Role</th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    {
+                                        adminStats.recentUsers?.map(recentUser => (
+                                            <tr key={recentUser._id}>
+                                                <td>{recentUser.displayName}</td>
+                                                <td>{recentUser.email}</td>
+                                                <td>{recentUser.role}</td>
+                                            </tr>
+                                        ))
+                                    }
+                                </tbody>
+                            </table>
+                        </div>
                     </div>
-                    <div className="overflow-x-auto no-scrollbar">
-                        <table className="table">
-                            {/* head */}
-                            <thead>
-                                <tr>
-                                    <th>Name</th>
-                                    <th>Email</th>
-                                    <th>Role</th>
-                                </tr>
-                            </thead>
-                            <tbody>
-                                {
-                                    adminStats.recentUsers?.map(recentUser => (
-                                        <tr key={recentUser._id}>
-                                            <td>{recentUser.displayName}</td>
-                                            <td>{recentUser.email}</td>
-                                            <td>{recentUser.role}</td>
-                                        </tr>
-                                    ))
-                                }
-                            </tbody>
-                        </table>
-                    </div>
-                </div>
+                </FadeIn>
             </div>
         </div>
     )
--- src/Pages/Dashboard/Dashboard/AdminDashboard/AdminStats/AdminStats.jsx
+++ src/Pages/Dashboard/Dashboard/AdminDashboard/AdminStats/AdminStats.jsx
@@ -32,19 +32,19 @@ const AdminStats = () => {
 
     return (
         <div className="grid md:grid-cols-3 gap-3 items-stretch content-stretch p-5 border border-base-100 bg-base-300/45 rounded-2xl shadow-sm shadow-base-100">
-            <FadeIn className="h-full" duration={1} delay={0.5}>
+            <FadeIn className="h-full" duration={1} delay={0.6}>
                 <div className="flex flex-col justify-center items-center h-full text-center p-2 md:p-5 border border-base-100 bg-base-100 rounded-xl shadow-sm hover:bg-primary hover:text-white hover:border-primary">
                     <h1 className="font-bold md:text-xl">Total Users</h1>
                     <p className="font-bold text-xl">{users.length}</p>
                 </div>
             </FadeIn>
-            <FadeIn className="h-full" duration={1} delay={0.6}>
+            <FadeIn className="h-full" duration={1} delay={0.7}>
                 <div className="flex flex-col justify-center items-center h-full text-center p-2 md:p-5 border border-base-100 bg-base-100 rounded-xl shadow-sm hover:bg-primary hover:text-white hover:border-primary">
                     <h1 className="font-bold md:text-xl">Total Applications</h1>
                     <p className="font-bold text-xl">{applications.length}</p>
                 </div>
             </FadeIn>
-            <FadeIn className="h-full" duration={1} delay={0.7}>
+            <FadeIn className="h-full" duration={1} delay={0.8}>
                 <div className="flex flex-col justify-center items-center h-full text-center p-2 md:p-5 border border-base-100 bg-base-100 rounded-xl shadow-sm hover:bg-primary hover:text-white hover:border-primary">
                     <h1 className="font-bold md:text-xl">Total Loans</h1>
                     <p className="font-bold text-xl">{loans.length}</p>
